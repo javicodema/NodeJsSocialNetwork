6fc2d1eb3b3ffba3c7894c12c9a7ac1e5c1eb65c
Javier Díez García <javi_codema@hotmail.com>
2018-04-10 22:03:34 +0200

    Server already working

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var express = require('express');
+var mongo = require('mongodb');
+var app = express();
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+var crypto = require('crypto');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-// view engine setup
+// Variables
+app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@ds241489.mlab.com:41489/socialnetwork');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
@@ -21,6 +34,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static('public'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +60,9 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+});
+
+
